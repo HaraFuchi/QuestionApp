92303f572c5d60f284afa1e0a43d826e7cf2495a
Ayano Hara <ayano0127@gmail.com>
2020-08-28 16:02:13 +0900

    first commit

---
+++ QuestionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionApp
+//
+//  Created by AYANO HARA on 2020/08/28.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
