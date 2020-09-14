aeb08cd438e94190f11b4320fb20d4948b78e458
Ayano Hara <ayano0127@gmail.com>
2020-09-14 14:35:24 +0900

    RegisterQuestionsVC作成

---
+++ QuestionApp/Controller/RegisterQuestionsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterQuestionsViewController.swift
+//  QuestionApp
+//
+//  Created by AYANO HARA on 2020/09/14.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import UIKit
+
+class RegisterQuestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
