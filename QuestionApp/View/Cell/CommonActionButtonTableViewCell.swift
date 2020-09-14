74e376176572ef04d30114eb19109d8c41d797ca
Ayano Hara <ayano0127@gmail.com>
2020-09-14 13:44:47 +0900

    ボタンのカスタムセル作成とAutoLayout

---
+++ QuestionApp/View/Cell/CommonActionButtonTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  CommonActionButtonTableViewCell.swift
+//  QuestionApp
+//
+//  Created by AYANO HARA on 2020/09/14.
+//  Copyright © 2020 AYANO HARA. All rights reserved.
+//
+
+import UIKit
+
+class CommonActionButtonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.selectionStyle = .none
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
