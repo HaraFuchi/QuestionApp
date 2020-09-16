b407038065a38755abc212032cef669925120676
Ayano Hara <ayano0127@gmail.com>
2020-09-16 22:36:41 +0900

    カスタムセルを反映

--- QuestionApp/Controller/HomeViewController.swift
+++ QuestionApp/Controller/HomeViewController.swift
@@ -8,23 +8,24 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        let homeQuestionNib = UINib(nibName: "HomeQuestionTableViewCell", bundle: nil)
+        tableView.register(homeQuestionNib, forCellReuseIdentifier: "HomeQuestionCell")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let homeQuestionCell = tableView.dequeueReusableCell(withIdentifier: "HomeQuestionCell", for: indexPath) as! HomeQuestionTableViewCell
+        return homeQuestionCell
+    }
 }
